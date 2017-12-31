3ad7ec0ee6b0b31fd2e0dea2089c72ed049541ac
Dmitrii Goriunov <goriunovd@gmail.com>
2017-12-31 23:49:30 +1300

    Update ts-builder to rollup for better performance

---
+++ bin/ts-builder.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+const shell = require('shelljs')
+const path = require('path')
+process.cwd()
+
+const args = process.argv.slice()
+let crossEnv = 'cross-env'
+
+args.forEach(arg => {
+    switch (arg) {
+        case '-prod': return crossEnv += ' prod=true '
+        case '-npm': return crossEnv += ' npm=true '
+        case '-node': return crossEnv += ' isNode=true '
+        case '-name': return crossEnv += ' name=' + args[args.indexOf('-name') + 1] + ' '
+        case '-folder': return crossEnv += ' folder=' + args[args.indexOf('-folder') + 1] + ' '
+        case '-mainfile': return crossEnv += ' mainfile=' + args[args.indexOf('-mainfile') + 1] + ' '
+        case '-outfile': return crossEnv += ' outfile=' + args[args.indexOf('-outfile') + 1] + ' '
+        case '-outfolder': return crossEnv += ' outfolder=' + args[args.indexOf('-outfolder') + 1] + ' '
+        case '-format': return crossEnv += ' format=' + args[args.indexOf('-format') + 1] + ' '
+        default: break
+    }
+})
+
+shell.exec(crossEnv + ' node ./node_modules/ts-builder/rollup.config.js --color')
---
+++ rollup.config.js
@@ -0,0 +1,102 @@
+const fs = require('fs')
+const path = require('path')
+const rollup = require('rollup').rollup
+const uglify = require('rollup-plugin-uglify')
+const filesize = require('rollup-plugin-filesize')
+const typescriptPlugin = require('rollup-plugin-typescript2')
+
+
+const options = {
+    compilerOptions: {
+        target: "es5",
+        module: "commonjs",
+        removeComments: true,
+        moduleResolution: "node",
+        declaration: true,
+        declarationDir: "../../" + process.env.folder
+    },
+    files: [
+        "../@types/node/index.d.ts",
+        "../@types/uws/index.d.ts"
+    ],
+    include: [
+        "../../" + process.env.folder + "/**/*.ts"
+    ]
+}
+
+if (!process.env.npm) {
+    delete options.compilerOptions['declaration']
+    delete options.compilerOptions['declarationDir']
+}
+if (!process.env.isNode) {
+    delete options.compilerOptions['moduleResolution']
+    delete options['files']
+}
+
+const copyPlugin = function (options) {
+    return {
+        ongenerate() {
+            const targDir = path.dirname(options.targ);
+            if (!fs.existsSync(targDir)) {
+                fs.mkdirSync(targDir)
+            }
+            if (options.remove) {
+                let json = fs.readFileSync(options.src)
+                let parsed = JSON.parse(json)
+                options.remove.forEach(element => delete parsed[element])
+                fs.writeFileSync(options.targ, JSON.stringify(parsed, null, '\t'))
+            } else {
+                fs.writeFileSync(options.targ, fs.readFileSync(options.src))
+            }
+        }
+    }
+}
+
+return rollup({
+    input: './' + process.env.folder + '/' + process.env.mainfile,
+    plugins: [
+        typescriptPlugin({
+            useTsconfigDeclarationDir: true,
+            tsconfig: './node_modules/ts-builder/tsconfig.json',
+            cacheRoot: './node_modules/ts-builder/cache',
+            tsconfigOverride: options
+        }),
+        uglify({
+            mangle: true,
+            output: {
+                beautify: process.env.prod ? false : true
+            }
+        }),
+        !process.env.npm || copyPlugin({
+            src: './package.json',
+            targ: './' + process.env.outfolder + '/package.json',
+            remove: ['devDependencies', 'scripts']
+        }),
+        !process.env.npm || copyPlugin({
+            src: './README.md',
+            targ: './' + process.env.outfolder + '/README.md',
+        }),
+        !process.env.npm || copyPlugin({
+            src: './LICENSE',
+            targ: './' + process.env.outfolder + '/LICENSE',
+        }),
+        filesize()
+    ],
+    external: ['cluster', 'http', 'https', 'uws']
+}).then((bundle) => {
+    bundle.write({ format: process.env.format || 'cjs', file: './' + process.env.outfolder + '/' + process.env.outfile, name: process.env.name || 'ClusterWS' }).then(() => {
+        if (process.env.npm) {
+            const dts = require('dts-bundle')
+            dts.bundle({
+                externals: false,
+                referenceExternals: false,
+                name: "index",
+                main: './' + process.env.folder + '/**/*.d.ts',
+                out: '../' + process.env.outfolder + '/index.d.ts',
+                removeSource: true,
+                outputAsModuleFolder: true,
+                emitOnIncludedFileNotFound: true
+            })
+        }
+    })
+})
